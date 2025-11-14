a9fa5a5de377982869300746de1b303a15e99899
ns9121212 <ns9121212@gmail.com>
2025-11-14 08:04:42 +0600

    online status and timezone

--- Client.py
+++ Client.py
@@ -1,8 +1,12 @@
 import threading
 import socket
-alias = input('choose and alias>>')
+alias = input('Choose an alias >>> ')
+timezone = input('Enter your timezone (e.g. +02:00 or leave empty for UTC+06:00) >>> ').strip()
+if not timezone:
+    timezone = 'UTC+06:00'
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1',59374))
+client.connect(('127.0.0.1', 59374))
+
 
 def client_receive():
     while True:
@@ -10,21 +14,26 @@ def client_receive():
             message = client.recv(1024).decode('utf-8')
             if message == "alias?":
                 client.send(alias.encode('utf-8'))
+            elif message == 'timezone?':
+                client.send(timezone.encode('utf-8'))
+            elif message.startswith('ONLINE:'):
+                print(f'\n[{message}]')
             else:
                 print(message)
         except:
-            print('An error occured!')
+            print('Error!')
             client.close()
             break
+
+
 def client_send():
     while True:
         message = f'{alias}: {input("")}'
         client.send(message.encode('utf-8'))
-receive_thread = threading.Thread(target = client_receive)
+
+
+receive_thread = threading.Thread(target=client_receive)
 receive_thread.start()
 
-send_thread = threading.Thread(target = client_send)
+send_thread = threading.Thread(target=client_send)
 send_thread.start()
-
-
-
--- Server.py
+++ Server.py
@@ -3,14 +3,23 @@ import socket
 host = '127.0.0.1'
 port = 59374
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host,port))
+server.bind((host, port))
 server.listen()
 clients = []
 aliases = []
+timezones = []
+
+
+def build_online_list():
+    if not aliases:
+        return "ONLINE: (no connected users)"
+    return "ONLINE: " + ", ".join([f"{a} ({tz})" for a, tz in zip(aliases, timezones)])
+
 
 def broadcast(message):
     for client in clients:
         client.send(message)
+
 
 def handle_client(client):
     while True:
@@ -22,28 +31,33 @@ def handle_client(client):
             clients.remove(client)
             client.close()
             alias = aliases[index]
-            broadcast(f'{alias}has left the chat'.encode('utf-8'))
+            broadcast(f'{alias} has left the chat room!'.encode('utf-8'))
             aliases.remove(alias)
+            timezones.pop(index)
+            broadcast(build_online_list().encode('utf-8'))
             break
 
-def recieve():
+def receive():
     while True:
-        print('server is running')
+        print('Server is running and listening ...')
         client, address = server.accept()
         print(f'connection is established with {str(address)}')
         client.send('alias?'.encode('utf-8'))
-        alias = client.recv(1024)
+        alias = client.recv(1024).decode('utf-8').strip()
+        client.send('timezone?'.encode('utf-8'))
+        tz = client.recv(1024).decode('utf-8').strip()
+        if not tz:
+            tz = 'UTC+06:00'
         aliases.append(alias)
+        timezones.append(tz)
         clients.append(client)
-        print(f'the alias of this client is {alias}'.encode('utf-8'))
-        broadcast(f'{alias} has joined the chat'.encode('utf-8'))
-        client.send('You are now connected!'.encode('utf-8'))
-        thread = threading.Thread(target=handle_client, args=(client))
+        print(f'The alias of this client is {alias} with timezone {tz}')
+        broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
+        broadcast(build_online_list().encode('utf-8'))
+        client.send('you are now connected!'.encode('utf-8'))
+        thread = threading.Thread(target=handle_client, args=(client,))
         thread.start()
-if __name__ == '__main__':
-    recieve()
 
 
-
-
-
+if __name__ == "__main__":
+    receive()
